26858ef74db11a7df248a6cdc49786b7d762c80d
antoinebqt <antoine.buquet49@gmail.com>
2020-03-03 17:44:14 +0100

    Choix langue de depart et traduction

--- ProjetISN/src/packageISN/main.java
+++ ProjetISN/src/packageISN/main.java
@@ -13,15 +13,28 @@ public class main {
 		String tab[][] = {{"Voiture","Chat","Visage"},
 				{"Car","Cat","Face"}};
 
+		//Choix de la langue de départ
+		int langue1;
+		int langue2;
+		System.out.println("Langue de départ ? (Français ou Anglais)");
+		String langue = sc.nextLine();
+		if (langue.equals("Français")){
+			langue1 = 0;
+			langue2 = 1;
+		}
+		else {
+			langue1 = 1;
+			langue2 = 0;
+		}
 		//Nombre aléatoire
 		int NbR = (int) (Math.round(Math.random()*2));;
 
 		//Mot à traduire
-		String mot1 = tab[0][NbR];
-		System.out.println("Le mot à traduire en anglais : " + mot1);
+		String mot1 = tab[langue1][NbR];
+		System.out.println("Le mot à traduire : " + mot1 + " (Attention, les majuscules et accents comptent !)");
 
 		//Mot traduis
-		String mot2 = tab[1][NbR];
+		String mot2 = tab[langue2][NbR];
 
 		//Réponse
 		String Proposition;
